c08b58cdedc8731c1543cf5f43491454bf135e74
0Nikola0 <nikoladesigns3@gmail.com>
2020-10-04 21:56:40 +0200

    Added player movement, started working on bricks

--- app.js
+++ app.js
@@ -20,22 +20,22 @@ class Paddle extends Rect{
         this.vel = new Vec(vel, 0)
     }
 
-    move(key){
-        switch (key.keyCode){
-            // right arrow
-            case 39: 
+    move(pos){
+        this.pos.x = pos
+    }
+
+    moveWithKeys(key, dt){
+        switch (key.code){
+            case "ArrowRight": 
                 this.pos.x += this.vel.x * (dt / 1000);
                 
-            // left arrow
-            case 37:
-                this.pos.x += this.vel.x * (dt / 1000);
+            case "ArrowLeft":
+                this.pos.x -= this.vel.x * (dt / 1000);
                 
-            // up arrow
-            case 38:
-                this.pos.y += this.vel.y * (dt / 1000);
+            case "ArrowUp":
+                this.pos.y -= this.vel.y * (dt / 1000);
                 
-            // down arrow
-            case 40:
+            case "ArrowDown":
                 this.pos.y += this.vel.y * (dt / 1000);
                 
         }
@@ -57,19 +57,22 @@ class Ball extends Rect{
     check_collision(/*Rect*/ objs){
         // If the ball hits left or right wall
         if ((this.pos.x < 0) || ((this.pos.x + this.size.x) >= Canvas.width)){
-            this.vel.x = -this.vel.x
+            this.vel.x *= -1;
+        }
+        // If the ball hits top wall
+        if (this.pos.y < 0){
+            this.vel.y *= -1;
         }
 
-        /*for (obj of objs){
+        for (let i = 0; i < objs.length; i++){
             // Check wheter the ball collides with any objs
-            if (((this.pos.y >= obj.pos.y) && (this.pos.y <= (obj.pos.y + obj.size.y))) && 
-                ((this.pos.x >= obj.pos.x) && (this.pos.x <= (obj.pos.x + obj.size.x)))){
+            if (((this.pos.y >= objs[i].pos.y) && (this.pos.y <= (objs[i].pos.y + objs[i].size.y))) && 
+                ((this.pos.x >= objs[i].pos.x) && (this.pos.x <= (objs[i].pos.x + objs[i].size.x)))){
                 // Change direction
-                this.vel.x = -this.vel.x;
-                this.vel.y = -this.vel.y;
+                this.vel.y *= -1;
                 break;
             }
-        }*/
+        }
     }
 
     move(dt){
@@ -84,12 +87,49 @@ class Ball extends Rect{
 }
 
 
+class Brick extends Rect{
+    constructor(pos, size=[50, 30]){
+        super(pos[0], pos[1], size[0], size[1]);
+    }
+
+    draw(){
+        context.fillStyle = "#fff";
+        context.fillRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
+    }
+}
+
+
+function moveWithMouse(/*Obj must have move() method*/ obj){
+    document.addEventListener("mousemove", event =>{
+        /*  the pos of the player is equal to the pos of the mouse minus the width of the player
+            so the player is centered to the mouse not left allignt,
+            unless the the player is about to exit the canvas
+        */
+        obj.move((event.offsetX) < Canvas.width - player.size.x ? event.offsetX - player.size.x / 2 : Canvas.width - player.size.x);
+    });
+}
+
+
+// TODO Fiiiix
+function create_bricks(num) {
+    let posx = 10;
+    let posy = 10;
+    let bricks = [];
+    for(i = 0; i < num; i++){
+        bricks += new Brick([posx, posy]);
+        posx += 60;
+    }
+    return bricks;
+}
+
+
 const Canvas = document.getElementById("game");
 const context = Canvas.getContext("2d");
 
-const player = new Paddle([Canvas.width / 2, Canvas.height - 30], [50, 20], 200);
-const ball = new Ball([Canvas.width / 2, 400], [20, 20], 200);
-console.log(ball)
+const player = new Paddle([Canvas.width / 2, Canvas.height - 30], [80, 20], 10);
+const ball = new Ball([Canvas.width / 3, 400], [20, 20], 200);
+
+const bricks = create_bricks(5);
 
 
 let LastTime;
@@ -103,7 +143,12 @@ function CallBack(millis){
 
 
 function update(dt){
+    // TODO Fiiiiix
+    let objs = bricks + player;
+    ball.check_collision(bricks);
+
     ball.move(dt);
+    moveWithMouse(player);
 
     // Background
     context.fillStyle = "#000";
@@ -111,11 +156,10 @@ function update(dt){
 
     ball.draw();
     player.draw();
+    for (brick of bricks){
+        brick.draw();
+    }
 
-    // TODO this doesnt work
-    Canvas.addEventListener("keydown", function(e){
-        player.move(e)
-    });
 }
 
 CallBack();
